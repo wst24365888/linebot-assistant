c8536a08ed4dd92081758f69501fad62c9acfd08
wst24365888 <hansen0715.2@gmail.com>
2018-06-12 20:09:22 +0800

    add firefox

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ from linebot.models import (
 
 from google import google, images
 
-import os
-os.chdir("C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
-
 import random as rd
 
 import newton_separate
