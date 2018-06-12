1fc3388b925fac7b46187ef466dee952e3271a9d
wst24365888 <hansen0715.2@gmail.com>
2018-06-12 08:31:08 +0800

    update function two

--- app.py
+++ app.py
@@ -41,35 +41,22 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    messages = event.message.text
-    reply = '目前有以下功能哦~\n\n1. 抽數字\n輸入\'抽\'呼叫\n\n2. 多項式拆出一次式\n輸入\'拆\''    
+    cmd, messages = event.message.text.split()
+    reply = '目前有以下功能哦~\n\n1. 抽數字\n輸入\'抽 min,max\'\nex.\ninput: 抽 1,100\noutput: 87\n\n2. 多項式拆出一次式\n輸入\'拆 terms1,terms2,...\nex.\ninput: 拆 1,2,1\noutput: (x+1)\''    
 
-    if '早' in messages or '安' in messages or '嗨' in messages or  '你好' in messages or'hello' in messages or 'hi' in messages:
-        mode = 0
+    if '早' in cmd or '安' in cmd or '嗨' in cmd or  '你好' in cmd or'hello' in cmd or 'hi' in cmd:
         reply = '安安'
-    elif '抽' in messages:
-        mode = 1
-        reply = '輸入 min,max 即會從範圍裡抽一個數字\nex.\ninput: 1,100\noutput: 87'
-    elif '拆' in messages:
-        mode = 2
-        reply = '輸入多項式的各項係數並用逗號分開\nex.\ninput: 1,2,1\noutput: (x+1)'
-    else:
-        mode = 0
-
-    if mode == 1:
+    elif '抽' in cmd:
         min_num, max_num = messages.split(',')
         min_num = int(min_num)
         max_num = int(max_num)
         reply = '{}'.format(rd.randint(min_num,max_num))
-        mode = 0
-    elif mode == 2:
+    elif '拆' in cmd:
         reply = run_main(messages.split(','))
-        mode = 0
-        
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text = "{};{}".format(reply, mode)))
+        TextSendMessage(text = "{}".format(reply)))
 
 
 if __name__ == "__main__":
