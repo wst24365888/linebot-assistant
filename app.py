9b94b959367d3a2cf57f8c1a4ef5edc33aaee46a
wst24365888 <hansen0715.2@gmail.com>
2018-06-13 14:17:47 +0800

    test

--- app.py
+++ app.py
@@ -78,7 +78,12 @@ def handle_message(event):
 
     elif '找' in cmd:
 
-        data = url.urlopen(url.Request('https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags={}'.format(urllib.parse.quote_plus('{}'.format(messages))))).read().decode('utf-8')
+        if messages.isdigit() == True:
+            keyword = messages
+        else:
+            keyword = urllib.parse.quote_plus('{}'.format(messages)
+
+        data = url.urlopen(url.Request('https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags={}'.format(keyword)))).read().decode('utf-8')
 
         data_str = ''
 
