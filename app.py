223e17d727de6f3d81def31d537255b6702fc9ef
wsst24365888 <hansen0715.2@gmail.com>
2018-06-14 18:58:15 +0800

    test

--- app.py
+++ app.py
@@ -99,11 +99,13 @@ def handle_message(event):
 
         img_urls = re.findall('"ou":"(.*?)"', data)
 
-        line_bot_api.reply_message(
-            event.reply_token,
-            ImageSendMessage(
-            original_content_url=img_urls[n],
-            preview_image_url=img_urls[n]))
+        reply = img_urls[n]
+
+#        line_bot_api.reply_message(
+ #           event.reply_token,
+  #          ImageSendMessage(
+   #         original_content_url=img_urls[n],
+    #        preview_image_url=img_urls[n]))
         
         
     line_bot_api.reply_message(
