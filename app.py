7f2275917e119e09c8520e46c55f29900c3b0a40
wst24365888 <hansen0715.2@gmail.com>
2018-06-15 22:10:49 +0800

    test

--- app.py
+++ app.py
@@ -128,8 +128,8 @@ def handle_message(event):
                     preview_image_url=img_url))
 
     elif 'user' in cmd:
-        
-        profile = line_bot_api.get_profile(user_id)
+
+        profile = line_bot_api.get_profile(event.user_id)
 
         reply = '{}\n{}\n{}\n{}'.format(profile.display_name, profile.user_id, profile.picture_url, profile.status_message)
         
