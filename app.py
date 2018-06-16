86ff9d417501ce2413b9facdc236a147cd0618cd
wst24365888 <hansen0715.2@gmail.com>
2018-06-16 13:23:25 +0800

    test

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import *
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, TemplateSendMessage
+)
 
 import random as rd
 
