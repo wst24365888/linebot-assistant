abdc1b3bb43362018ab9f8f7615e7bd6ccf13caa
wst24365888 <hansen0715.2@gmail.com>
2018-06-17 11:09:08 +0800

    test

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ import random as rd
 
 import newton_separate
 
+import secret
+
 import urllib.request
 
 import urllib.parse
@@ -24,8 +26,8 @@ from user_agent import generate_user_agent
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('ew9Gu+/a0OB/IT90r8mEiLaipylgz85Kw9maa8624PWPZsvnggQrt1iEbkMaPXFyJD+u6P3zmvMYiY3k3fu0+/c6lGZTcpG0AdeUs+ChuJ00knWPevFv2Jxnjnv6b+J9BmZkBGO5Zsms74pn42KasAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('c90f608354cca2df82c4e2b6167097c9')
+line_bot_api = LineBotApi(secret.line_bot_api())
+handler = WebhookHandler(secret.handler())
 
 
 @app.route("/callback", methods=['POST'])
