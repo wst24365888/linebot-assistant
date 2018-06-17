1f6a6e84a0eb72fbd17b26dc806a9b5408b88254
wst24365888 <hansen0715.2@gmail.com>
2018-06-17 11:34:48 +0800

    test

---
+++ secret.py
@@ -0,0 +1,11 @@
+def line_bot_api():
+    return 'ew9Gu+/a0OB/IT90r8mEiLaipylgz85Kw9maa8624PWPZsvnggQrt1iEbkMaPXFyJD+u6P3zmvMYiY3k3fu0+/c6lGZTcpG0AdeUs+ChuJ00knWPevFv2Jxnjnv6b+J9BmZkBGO5Zsms74pn42KasAdB04t89/1O/w1cDnyilFU='
+
+def handler():
+    return 'c90f608354cca2df82c4e2b6167097c9'
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    main()
